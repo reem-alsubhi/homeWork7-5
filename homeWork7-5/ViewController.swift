49f336a50d04c3f739049c2e449aae7e9c8744cc
reem alsubhi <Reem.Alsubi.med.s@tuwaiq.edu.sa>
2021-11-15 08:57:43 +0300

    Initial Commit

---
+++ homeWork7-5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWork7-5
+//
+//  Created by MAC on 10/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
